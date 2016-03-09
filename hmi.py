054ce26aaac74a46216abd17f757c925db729543
Florian <f.bitard@gmail.com>
2016-03-09 14:30:55 +0100

    [Pprz Center HMI model] Corrections in HMI class declaration to deal with inheritance problems.

--- hmi.py
+++ hmi.py
@@ -18,19 +18,19 @@ MODULE = '[HMI]'
 
 class Hmi(Widgets.QMainWindow):
     """Class to manage the main window HMI."""
-    def __init__(self, parent=None):
-        super(Hmi, self).__init__(parent)
+    def __init__(self):
+        super(Hmi, self).__init__()
         self.ui = ui.Ui_MainWindow()
         self.ui.setupUi(self)
 
-        self.settings = SettingsManager()
+        self.settings = SettingsManager(self)
 
         self.ui.actionSetManager.triggered.connect(self.settings.show)
 
 
 class SettingsManager(Widgets.QDialog):
     """Class to manage the settings manager HMI."""
-    def __init__(self, parent=None):
+    def __init__(self, parent):
         super(SettingsManager, self).__init__(parent)
         self.ui = manager.Ui_Dialog()
         self.ui.setupUi(self)
